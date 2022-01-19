6df405cc5875f43505fa685b1a5a35b9a2b4a444
inbar <im0804@bs.amalnet.k12.il>
2022-01-19 09:27:01 +0200

    end of project

--- app/src/main/java/com/example/clickex3/MainActivity.java
+++ app/src/main/java/com/example/clickex3/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.clickex3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    int click_counter = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
     }
-}
+
+    public void go(View view) {
+        click_counter++;
+        btn.setText("this is a click number: " + click_counter);
+        if (click_counter == 6){
+            btn.setText("Enough to click. Go to new start!");
+            click_counter = 0;
+        }
+    }
+    }
